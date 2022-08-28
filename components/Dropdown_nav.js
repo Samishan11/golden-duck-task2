7acc08e43f23669933a461732576c4e69dbf267b
Samishan <samishanthapa0@gmail.com>
2022-08-28 18:01:13 +0545

    changes

--- components/Dropdown_nav.js
+++ components/Dropdown_nav.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Style from "../public/static/dropdown_nav.module.css"
-import { Link } from "react-scroll";
+// import { Link } from "react-router-dom";
 const Dropdown_nav = ({ hoverShow }) => {
     return (
         <div className={hoverShow === true ? Style.contianer_show : Style.contianer}>
             <div className={Style.main}>
                 <div className={Style.line}></div>
                 <ul className={Style.links}>
-                    <li className={Style.li}><Link to='/webdevelopment' style={{ color: "#0477bf" }}>Web Development</Link></li>
-                    <li className={Style.li}><Link to='/degitalmarketing'>Web Development</Link></li>
-                    <li className={Style.li}><Link to='/graphicdesign'>Web Development</Link></li>
+                    <li className={ Style.li}><a href='/services/web-development' style={{ color: "#0477bf" }}>Web Development</a></li>
+                    <li className={Style.li}><a href='/degitalmarketing'>Web Development</a></li>
+                    <li className={Style.li}><a href='/graphicdesign'>Web Development</a></li>
                 </ul>
             </div>
         </div>
