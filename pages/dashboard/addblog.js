27a662384c8bff24590a17321fd5b90354890b59
Samishan11 <samishanthapa0@gmail.com>
2022-09-04 16:19:26 +0545

    blog

--- pages/dashboard/addblog.js
+++ pages/dashboard/addblog.js
@@ -4,39 +4,60 @@ import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.css';
 import { useQuill } from 'react-quilljs';
 import 'quill/dist/quill.snow.css';
-
-
+import axios from "axios";
+import Blog from "./blog"
 const Addblog = () => {
     const { quill, quillRef } = useQuill();
-    const [value, setValue] = useState();
+    const [description, setDescription] = useState();
+
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState('');
+
+
 
     React.useEffect(() => {
         if (quill) {
             quill.on('text-change', () => {
-                console.log(quillRef.current.firstChild.innerHTML);
-                setValue(quillRef.current.firstChild.innerHTML)
+                setDescription(quillRef.current.firstChild.innerHTML)
             });
         }
     }, [quill]);
-    console.log(value)
+
+    // add blog
+    var fd = new FormData();
+    fd.append("title", title)
+    fd.append("description", description)
+    fd.append("image", image)
+
+    const addBlog = async () => {
+        try {
+            const res = await axios.post("http://localhost:8000/api/v4/blog/post", fd)
+            console.log(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     return (
         <div style={{ padding: "1rem" }}>
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Blog Title</Form.Label>
-                    <Form.Control type="email" placeholder="Enter blog title" />
+                    <Form.Control onChange={e => setTitle(e.target.value)} type="" placeholder="Enter blog title" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Blog Image</Form.Label>
-                    <Form.Control type="file" placeholder="" />
+                    <Form.Control onChange={e => setImage(e.target.files[0])} type="file" placeholder="" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Blog Description</Form.Label>
                     <div style={{ height: "10rem" }} ref={quillRef} />
                 </Form.Group>
             </Form>
-            <Button variant="outline-primary">Add Blog</Button>
+            <Button onClick={addBlog} variant="outline-primary">Add Blog</Button>
         </div>
     )
 }
--- pages/dashboard/blog.js
+++ pages/dashboard/blog.js
@@ -3,43 +3,81 @@ import Style from "../../public/static/adminblog.module.css";
 import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
-import {useState} from "react"
+import { useState } from "react"
 import Link from "next/link"
 import Addblog from './addblog';
+import axios from "axios";
+
 function blog() {
 
-    const [blog,setBlog] = useState(false);
+  const [blog, setBlog] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  const [change, setChange] = useState(false)
+
+  // get blogs
+  React.useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const res = await axios.get("https://golden-duck-it.herokuapp.com/api/v4/blog")
+        setBlogs(res.data.data)
+
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getBlogs()
+  }, [change])
+
+  // delete blog
+  const deleteBlog = async (id) => {
+    try {
+      const res = await axios.delete(`https://golden-duck-it.herokuapp.com/api/v4/blog/delete/${id}`)
+      if (change) {
+        setChange(false)
+      } else {
+        setChange(true)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
 
 
   return (
     <div className='body' style={{ padding: "1rem" }}>
-        {!blog &&  <Button variant="outline-success" onClick={setBlog.bind(this,true)} className='link' smooth={true} style={{textDecoration:"none"}} >Add Blog</Button>}
-     
-        {blog ? <Addblog/> :  <div style={{ marginTop: "1rem" }} className="blogs">
+      {!blog && <Button variant="outline-success" onClick={setBlog.bind(this, true)} className='link' smooth={true} style={{ textDecoration: "none" }} >Add Blog</Button>}
+
+      {blog ? <Addblog /> : <div style={{ marginTop: "1rem" }} className="blogs">
         <Table striped bordered hover>
           <thead>
             <tr>
               <th>#</th>
               <th>Blog Title</th>
-              <th>Description</th>
               <th>Aurthor</th>
               <th>Date</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>@mdo</td>
-              <td> <Button variant="outline-success">Edit</Button>  <Button variant="outline-danger">Delete</Button></td>
-            </tr>
+            {
+              blogs.map((data, ind) => {
+                return (
+                  <tr>
+                    <td>{ind+1}</td>
+                    <td>{data.title}</td>
+                    <td>Admin</td>
+                    <td>{data.date}</td>
+                    <td> <Button variant="outline-success">Edit</Button>  <Button onClick={deleteBlog.bind(this,data._id)} variant="outline-danger">Delete</Button></td>
+                  </tr>
+                )
+              })
+            }
+
           </tbody>
         </Table>
       </div>}
-     
+
     </div>
 
   )
