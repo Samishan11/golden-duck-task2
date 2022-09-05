8335a8630aaf07565b6cbce9a4d7fbbc150f176e
Samishan11 <samishanthapa0@gmail.com>
2022-09-05 13:33:17 +0545

    Portfolio get

--- pages/Portfolio.js
+++ pages/Portfolio.js
@@ -4,7 +4,24 @@ import Sideicons from '../components/Sideicons'
 import Style from "../public/static/portfoli.module.css"
 import { BsFillPlusCircleFill } from 'react-icons/bs';
 import Footer from "../components/Footer"
+import axios from 'axios';
+
 const Portfolio = () => {
+    // get all portfolio
+    const [portfolio, setPortfolio] = React.useState([]);
+    React.useEffect(() => {
+        const getPortfolio = async () => {
+            try {
+                const res = await axios.get("https://golden-duck-it.herokuapp.com/api/v4/portfolio")
+                setPortfolio(res.data.data)
+                console.log(res.data.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getPortfolio()
+    }, [])
+
     return (
         <div className={Style.contianer}>
             <Navbar></Navbar>
@@ -26,71 +43,24 @@ const Portfolio = () => {
             </div>
             <div className={Style.branchmain}>
                 <div className={Style.branch}>
-                    <a href="/project/Project">
-                        <div className={Style.main}>
-                            <div className={Style.body}>
-                                <img src="/assets/images/asset 29.png" alt="JAC Motors" sizes="(max-width:479px) 479px, 100vw " />
-                                <BsFillPlusCircleFill className={Style.plusicon}></BsFillPlusCircleFill>
-                            </div>
-                            <div className="content">
-                                <p className={Style.companyname}>JAC Motors</p>
-                                <p className={Style.work}>Website Development</p>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="/project/Project">
-                        <div className={Style.main}>
-                            <div className={Style.body}>
-                                <img src="/assets/images/asset 30.jpg" alt="JAC Motors" sizes="(max-width:479px) 479px, 100vw " />
-                                <BsFillPlusCircleFill className={Style.plusicon}></BsFillPlusCircleFill>
-                            </div>
-                            <div className="content">
-                                <p className={Style.companyname}>JAC Motors</p>
-                                <p className={Style.work}>Website Development</p>
-                            </div>
-                        </div>
-                    </a>
-
-                    <div className={Style.main}>
-                        <div className={Style.body}>
-                            <img src="/assets/images/asset 29.png" alt="JAC Motors" sizes="(max-width:479px) 479px, 100vw " />
-                            <BsFillPlusCircleFill className={Style.plusicon}></BsFillPlusCircleFill>
-                        </div>
-                        <div className="content">
-                            <p className={Style.companyname}>JAC Motors</p>
-                            <p className={Style.work}>Website Development</p>
-                        </div>
-                    </div>
-                    <div className={Style.main}>
-                        <div className={Style.body}>
-                            <img src="/assets/images/asset 30.jpg" alt="JAC Motors" sizes="(max-width:479px) 479px, 100vw " />
-                            <BsFillPlusCircleFill className={Style.plusicon}></BsFillPlusCircleFill>
-                        </div>
-                        <div className="content">
-                            <p className={Style.companyname}>JAC Motors</p>
-                            <p className={Style.work}>Website Development</p>
-                        </div>
-                    </div>
-                    <div className={Style.main}>
-                        <div className={Style.body}>
-                            <img src="/assets/images/asset 29.png" alt="JAC Motors" sizes="(max-width:479px) 479px, 100vw " />
-                            <BsFillPlusCircleFill className={Style.plusicon}></BsFillPlusCircleFill>
-                        </div>
-                        <div className="content">
-                            <p className={Style.companyname}>JAC Motors</p>
-                            <p className={Style.work}>Website Development</p>
-                        </div>
-                    </div>
-                    <div className={Style.main}>
-                        <div className={Style.body}>
-                            <img src="/assets/images/asset 30.jpg" alt="JAC Motors" sizes="(max-width:479px) 479px, 100vw " />
-                            <BsFillPlusCircleFill className={Style.plusicon}></BsFillPlusCircleFill>
-                        </div>
-                        <div className="content">
-                            <p className={Style.companyname}>JAC Motors</p>
-                            <p className={Style.work}>Website Development</p>
-                        </div>
-                    </div>
+                    {
+                        portfolio.map(data => {
+                            return (
+                                <a href="/project/Project">
+                                    <div className={Style.main}>
+                                        <div className={Style.body}>
+                                            <img src={`http://localhost:8000/${data.image}`} alt="JAC Motors" sizes="(max-width:479px) 479px, 100vw " />
+                                            <BsFillPlusCircleFill className={Style.plusicon}></BsFillPlusCircleFill>
+                                        </div>
+                                        <div className="content">
+                                            <p className={Style.companyname}>{data.brand_name}</p>
+                                            <p className={Style.work}>{data.catagory}</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            )
+                        })
+                    }
                 </div>
             </div>
             <div className={Style.line} />
