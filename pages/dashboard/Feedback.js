08a46d7924d3dd0cea4ba4914e938865427b03f5
RohanKumarMainali <rohanmainali39@gmail.com>
2022-09-06 13:37:19 +0545

    feedback completed

--- pages/dashboard/Feedback.js
+++ pages/dashboard/Feedback.js
@@ -3,28 +3,170 @@ import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Cookies from 'js-cookie';
 
 function feedback() {
 
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [author, setAuthor] = useState();
+  const [image, setImage] = useState();
 
-    const [title,setTitle] = useState();
-    const [description,setDescription] = useState();
-    const [author,setAuthor] = useState();
-    const [action,setAction] = useState();
-    const[feedbacks,setFeedbacks] = useState([]);
+  const [show, setShow] = useState(false);
+  const [show1, setShow1] = useState(false);
 
-    const getFeedback = async () =>{
-        const response = await axios.get('https://golden-duck-it.herokuapp.com/api/v4/feedback');
-        console.log(response.data.feedbacks);
-        setFeedbacks(response.data.feedbacks)
+  //for edit
+  const [singleFeedback, setSingleFeedback] = useState({});
+  const [singleFeedbackTitle, setSingleFeedbackTitle] = useState();
+  const [singleFeedbackDescription, setSingleFeedbackDescription] = useState();
+  const [singleFeedbackImage, setSingleFeedbackImage] = useState();
+  const [singleFeedbackAuthor, setSingleFeedbackAuthor] = useState();
+  const [singleFeedbackId, setSingleFeedbackId] = useState();
+
+  //--------------------------------------------------------
+
+
+  const [change, setChange] = useState(false)
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleClose1 = () => setShow1(false);
+  const handleShow1 = () => setShow1(true);
+
+  const [action, setAction] = useState();
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  const [showEdit, setEditShow] = useState(false);
+
+  const handleEditShow = (i) => {
+    setEditShow(true)
+
+    console.log(feedbacks[i])
+    setSingleFeedback(feedbacks[i]);
+    setSingleFeedbackTitle(feedbacks[i].title)
+    setSingleFeedbackDescription(feedbacks[i].description)
+    setSingleFeedbackAuthor(feedbacks[i].author)
+    setSingleFeedbackImage(feedbacks[i].image)
+    setSingleFeedbackId(feedbacks[i]._id)
+
+
+  }
+
+  //------------------------------
+
+  const handleEditClose = () => setEditShow(false)
+
+  const getFeedback = async () => {
+    const response = await axios.get('https://golden-duck-it.herokuapp.com/api/v4/feedback');
+    console.log(response.data.feedbacks);
+    setFeedbacks(response.data.feedbacks)
+  }
+
+  // add Feedback
+  const addFeedback = async () => {
+    let fd = new FormData();
+    fd.append("title", title)
+    fd.append("description", description)
+    fd.append("author", author)
+    fd.append("image", image)
+
+    try {
+      const res = await axios.post("https://golden-duck-it.herokuapp.com/api/v4/postFeedback", fd)
+      console.log(res)
+      if (change) {
+        setChange(false)
+      } else {
+        setChange(true)
+      }
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    useEffect  (() =>{
-        getFeedback()
-    },[])
+  // edit Feedback
+  const editFeedback = async () => {
+    const editFormData = new FormData();
+    editFormData.append('feedBackId', singleFeedbackId)
+    editFormData.append('title', singleFeedbackTitle);
+    editFormData.append('description', singleFeedbackDescription);
+    editFormData.append('author', singleFeedbackAuthor);
+    editFormData.append('image', singleFeedbackImage);
+    try {
+      const res = await axios.put(`https://golden-duck-it.herokuapp.com/api/v4/editFeedback`, editFormData)
+      if (change) {
+        setChange(false)
+      } else {
+        setChange(true)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
+  //  delete Feedback
+  const deleteFeedback = async (i) => {
+    try {
+      const res = await axios.delete(`https://golden-duck-it.herokuapp.com/api/v4/deleteFeedback/${feedbacks[i]._id}`)
+      console.log(res)
+      if (change) {
+        setChange(false)
+      } else {
+        setChange(true)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getFeedback()
+  }, [change])
 
   return (
-      <div className='body'>
+    <div className='body'>
+      <Button onClick={handleShow} variant="outline-success" className='link' smooth={true} style={{ textDecoration: "none" }} >Add Feedback</Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Feedback Title</Form.Label>
+              <Form.Control onChange={e => setTitle(e.target.value)} type="" placeholder="Enter Feedback title" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Author</Form.Label>
+              <Form.Control onChange={e => setAuthor(e.target.value)} type="" placeholder="Enter Author Name " />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Feedback Description</Form.Label>
+              <Form.Control onChange={e => setDescription(e.target.value)} type="" placeholder="Enter Description" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Feedback Image</Form.Label>
+              <Form.Control onChange={e => setImage(e.target.files[0])} type="file" placeholder="" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button onClick={addFeedback} variant="primary">Add Feedback</Button>
+        </Modal.Footer>
+      </Modal>
       <div className="blogs">
         <Table striped bordered hover>
           <thead>
@@ -38,25 +180,62 @@ function feedback() {
             </tr>
           </thead>
           <tbody>
-            {feedbacks.map((feedback, index)=>{
-                return (    
-                <tr><td>{index+1}</td>
+            {feedbacks.map((feedback, index) => {
+              return (
+                <tr><td>{index + 1}</td>
 
-                    <td>{feedback.title}</td>
-                    <td>{feedback.description}</td>
-                    <td>{feedback.author}</td>
-                    <td>2020 : 12: 01</td>
-                    <td> <Button variant="outline-success">Edit</Button>  <Button variant="outline-danger">Delete</Button></td>
-                  </tr>
-                
-                )
+                  <td>{feedback.title}</td>
+                  <td>{feedback.description}</td>
+                  <td>{feedback.author}</td>
+                  <td>2020 : 12: 01</td>
+                  <td> <Button variant="outline-success" onClick={handleEditShow.bind(this, index)} >Edit</Button>
+                    <Button variant="outline-danger" onClick={deleteFeedback.bind(this, index)}>Delete</Button></td>
+                </tr>
+
+              )
             })}
-            
+
           </tbody>
         </Table>
+        <Modal
+          show={showEdit}
+          onHide={handleEditClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Feedback Title</Form.Label>
+                <Form.Control value={singleFeedbackTitle} onChange={e => setSingleFeedbackTitle(e.target.value)} type="" placeholder="Enter Feedback title" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Author</Form.Label>
+                <Form.Control value={singleFeedbackAuthor} onChange={e => setSingleFeedbackAuthor(e.target.value)} type="" placeholder="Enter Feedback Author" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Feedback Description</Form.Label>
+                <Form.Control value={singleFeedbackDescription} onChange={e => setSingleFeedbackDescription(e.target.value)} type="" placeholder="Enter Feedback catagory" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Feedback Image</Form.Label>
+                <Form.Control onChange={(e) => setSingleFeedbackImage(e.target.files[0])} type="file" placeholder="" />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleEditClose}>
+              Close
+            </Button>
+            <Button onClick={editFeedback} variant="primary">Update Feedback</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
-        
+
   )
 }
 
--- pages/dashboard/Project.js
+++ pages/dashboard/Project.js
@@ -11,153 +11,180 @@ import axios from "axios";
 function Project() {
 
 
-    const[projects,setProjects] = useState([]);
-    const[singleProject,setSingleProject] =  useState({});
-    const[singleProjectTitle,setSingleProjectTitle] =  useState();
-    const[singleProjectCategory,setSingleProjectCategory] =  useState();
-    const[singleProjectImage,setSingleProjectImage] =  useState();
-    const[singleProjectLink,setSingleProjectLink] =  useState();
-    const[singleProjectId,setSingleProjectId] =  useState();
+  const [projects, setProjects] = useState([]);
+  const [link, setLink] = useState();
+  const [category,setCategory] = useState();
 
-    const[projectDeleteId,setProjectDeleteId] = useState();
+  const [singleProject, setSingleProject] = useState({});
+  const [singleProjectTitle, setSingleProjectTitle] = useState();
+  const [singleProjectCategory, setSingleProjectCategory] = useState();
+  const [singleProjectImage, setSingleProjectImage] = useState();
+  const [singleProjectLink, setSingleProjectLink] = useState();
+  const [singleProjectId, setSingleProjectId] = useState();
+  const [change, setChange] = useState(false)
+  const [projectDeleteId, setProjectDeleteId] = useState();
+ 
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleEditShow = (i) => {
+    setEditShow(true)
+
+    console.log(projects[i])
+    setSingleProject(projects[i]);
+    setSingleProjectTitle(projects[i].title)
+    setSingleProjectLink(projects[i].link)
+
+    setSingleProjectCategory(projects[i].category)
+    setSingleProjectImage(projects[i].image)
+    setSingleProjectId(projects[i]._id)
 
 
-    const [show, setShow] = useState(false);
-    const [showEdit, setEditShow] = useState(false);
+  }
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  //------------------------------
 
-    const handleEditShow = (i) =>{ setEditShow(true)
-
-      console.log(projects[i])
-      setSingleProject(projects[i]);
-      setSingleProjectTitle(projects[i].title)
-      setSingleProjectLink(projects[i].link)
-
-      setSingleProjectCategory(projects[i].category)
-      setSingleProjectImage(projects[i].image)
-      setSingleProjectId(projects[i]._id)
+  const handleEditClose = () => setEditShow(false)
 
 
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [image, setImage] = useState();
+
+  const token = Cookies.get('accessToken');
+  console.log(token);
+
+
+
+
+  //------------------------------------------------
+
+  const getProject = async () => {
+    try {
+      const response = await axios.get('https://golden-duck-it.herokuapp.com/api/v4/getProject');
+   
+      setProjects(response.data.projects)
+    } catch (err) {
+      console.log(err.message)
     }
 
-    //------------------------------
 
-    const handleEditClose = () => setEditShow(false)
-
-
-    const [title,setTitle] = useState();
-    const [description,setDescription] = useState();
-    const [image, setImage] =  useState();
-
-    const token = Cookies.get('accessToken');
-    console.log(token);
+  }
 
 
 
-
-    //------------------------------------------------
-
-    const getProject = async () =>{
-      
-        const response = await axios.get('https://golden-duck-it.herokuapp.com/api/v4/getProject');
-        console.log(response);
-        console.log(response.data.projects)
-        setProjects(response.data.projects)
-    }
-
-    useEffect  (() =>{
-        getProject();
-    },[])
-
-    const formData = new FormData();
-    formData.append("title",title);
-    formData.append("description",description);
-    formData.append("image",image);
-
-    const editFormData = new FormData();
-    editFormData.append('projectId',singleProjectId)
-    editFormData.append('title',singleProjectTitle);
-    editFormData.append('category',singleProjectCategory);
-    editFormData.append('image',singleProjectImage);
-    editFormData.append('link',singleProjectLink);
-    
+  const formData = new FormData();
+  formData.append("title", title);
+  formData.append("category", category);
+  formData.append("image", image);
+  formData.append("link", link);
 
 
-    const addProject =async (e) =>{
+  const editFormData = new FormData();
+  editFormData.append('projectId', singleProjectId)
+  editFormData.append('title', singleProjectTitle);
+  editFormData.append('category', singleProjectCategory);
+  editFormData.append('image', singleProjectImage);
+  editFormData.append('link', singleProjectLink);
 
-      e.preventDefault();
 
+
+  const addProject = async (e) => {
+
+    e.preventDefault();
+    try {
       const response = await axios.post('https://golden-duck-it.herokuapp.com/api/v4/postProject',
-      formData,
+        formData,
       );
+      if (change) {
+        setChange(false)
+      } else {
+        setChange(true)
+      }
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    const editProject = async (e) =>{
+  const editProject = async (e) => {
 
-      console.log(editFormData)
-      try{
-        const response = await axios.put('https://golden-duck-it.herokuapp.com/api/v4/editProject',
+    console.log(editFormData)
+    try {
+      const response = await axios.put('https://golden-duck-it.herokuapp.com/api/v4/editProject',
         editFormData
       )
-      }
-      catch(err){
-        console.log(err.message)
-      }
-    
-    }
-
-
-    const deleteProject = async (i) => {
-      setProjectDeleteId(projects[i]._id)
-
-      try{
-          const response =  await axios.delete('https://golden-duck-it.herokuapp.com/api/v4/deleteProject',
-            {projectDeleteId}
-          ) 
-          console.log(response);
-      }catch(err){
-        console.log(err.message)
+      if (change) {
+        setChange(false)
+      } else {
+        setChange(true)
       }
     }
+    catch (err) {
+      console.log(err.message)
+    }
+
+  }
+
+
+  const deleteProject = async (i) => {
+    try {
+      const response = await axios.delete(`https://golden-duck-it.herokuapp.com/api/v4/deleteProject/${projects[i]._id}`,
+      )
+      if (change) {
+        setChange(false)
+      } else {
+        setChange(true)
+      }
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
+  useEffect(() => {
+    getProject();
+  }, [change])
 
   return (
-      <div className='body'>
-            <Button onClick={handleShow} variant="outline-success" className='link' smooth={true} style={{ textDecoration: "none" }} >Add Project</Button>
+    <div className='body'>
+      <Button onClick={handleShow} variant="outline-success" className='link' smooth={true} style={{ textDecoration: "none" }} >Add Project</Button>
 
-        <Modal
-                show={show}
-                onHide={handleClose}
-                backdrop="static"
-                keyboard={false}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Project Title</Form.Label>
-                            <Form.Control onChange={e => setTitle(e.target.value)} type="" placeholder="Enter project title" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Project Category</Form.Label>
-                            <Form.Control onChange={e => setDescription(e.target.value)} type="" placeholder="Enter project catagory" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Project Image</Form.Label>
-                            <Form.Control onChange={e => setImage(e.target.files[0])} type="file" placeholder="" />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button onClick={addProject} variant="primary">Add Portfolio</Button>
-                </Modal.Footer>
-            </Modal>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Project Title</Form.Label>
+              <Form.Control onChange={e => setTitle(e.target.value)} type="" placeholder="Enter project title" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Project Category</Form.Label>
+              <Form.Control onChange={e => setCategory(e.target.value)} type="" placeholder="Enter project catagory" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Project link</Form.Label>
+              <Form.Control onChange={e => setLink(e.target.value)} type="" placeholder="Enter project catagory" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Project Image</Form.Label>
+              <Form.Control onChange={e => setImage(e.target.files[0])} type="file" placeholder="" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button onClick={addProject} variant="primary">Add Portfolio</Button>
+        </Modal.Footer>
+      </Modal>
       <div className="blogs">
         <Table striped bordered hover>
           <thead>
@@ -174,65 +201,65 @@ function Project() {
             </tr>
           </thead>
           <tbody>
-            {projects.map((project, index)=>{
-                return (    
-                <tr><td>{index+1}</td>
+            {projects.map((project, index) => {
+              return (
+                <tr><td>{index + 1}</td>
 
-                    <td>{project.title}</td>
-                    <td>{project.category}</td>
-                    <td><a>{project.link}</a></td>
+                  <td>{project.title}</td>
+                  <td>{project.category}</td>
+                  <td><a>{project.link}</a></td>
 
-                    <td>{project.image}</td>
-                    <td>2020 : 12: 01</td>
-                    <td> <Button variant="outline-success" onClick={handleEditShow.bind(this,index)} >Edit</Button>  
-                    <Button variant="outline-danger" onClick={deleteProject.bind(this,index)}>Delete</Button></td>
-                  </tr>
-                
-                )
+                  <td>{project.image}</td>
+                  <td>2020 : 12: 01</td>
+                  <td> <Button variant="outline-success" onClick={handleEditShow.bind(this, index)} >Edit</Button>
+                    <Button variant="outline-danger" onClick={deleteProject.bind(this, index)}>Delete</Button></td>
+                </tr>
+
+              )
             })}
-            
+
           </tbody>
         </Table>
 
         <Modal
-                show={showEdit}
-                onHide={handleEditClose}
-                backdrop="static"
-                keyboard={false}
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Project Title</Form.Label>
-                            <Form.Control value={singleProjectTitle}  onChange={e =>  setSingleProjectTitle(e.target.value)} type="" placeholder="Enter project title" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Project Category</Form.Label>
-                            <Form.Control value={singleProjectCategory} onChange={e => setSingleProjectCategory(e.target.value)} type="" placeholder="Enter project catagory" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Project Link</Form.Label>
-                            <Form.Control value={singleProjectLink} onChange={e => setSingleProjectLink(e.target.value)} type="" placeholder="Enter project link" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Project Image</Form.Label>
-                            <Form.Control  onChange={(e)=>setSingleProjectImage(e.target.files[0])} type="file" placeholder="" />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleEditClose}>
-                        Close
-                    </Button>
-                    <Button onClick={editProject} variant="primary">Add Portfolio</Button>
-                </Modal.Footer>
-            </Modal>
+          show={showEdit}
+          onHide={handleEditClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Project Title</Form.Label>
+                <Form.Control value={singleProjectTitle} onChange={e => setSingleProjectTitle(e.target.value)} type="" placeholder="Enter project title" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Project Category</Form.Label>
+                <Form.Control value={singleProjectCategory} onChange={e => setSingleProjectCategory(e.target.value)} type="" placeholder="Enter project catagory" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Project Link</Form.Label>
+                <Form.Control value={singleProjectLink} onChange={e => setSingleProjectLink(e.target.value)} type="" placeholder="Enter project link" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Project Image</Form.Label>
+                <Form.Control onChange={(e) => setSingleProjectImage(e.target.files[0])} type="file" placeholder="" />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleEditClose}>
+              Close
+            </Button>
+            <Button onClick={editProject} variant="primary">Add Portfolio</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
-        
+
   )
 }
 
